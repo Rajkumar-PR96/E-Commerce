50724fe65c40704ab19172652401453911ec71fa
Rajkumar-PR96 <161789329+Rajkumar-PR96@users.noreply.github.com>
2024-10-21 15:04:42 +0530

    Update ShopContext.jsx

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -18,12 +18,12 @@ const ShopContextProvider = (props) => {
   const [cartItems, setCartItems] = useState(getDefaultCart());
 
   useEffect(() => {
-     fetch('http://localhost:4000/allproducts')
+     fetch('https://e-commerce-backend-irdi.onrender.com/allproducts')
      .then((response) => response.json())
      .then((data) => setAll_Product(data))
 
      if(localStorage.getItem('auth-token')){
-      fetch('http://localhost:4000/getcart', {
+      fetch('https://e-commerce-backend-irdi.onrender.com/getcart', {
         method:'POST',
         headers:{
           Accept:'application/form-data',
@@ -42,7 +42,7 @@ const ShopContextProvider = (props) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
 
     if(localStorage.getItem('auth-token')){
-      fetch('http://localhost:4000/addtocart', {
+      fetch('https://e-commerce-backend-irdi.onrender.com/addtocart', {
         method:'POST',
         headers:{
           Accept:'application/form-data',
@@ -60,7 +60,7 @@ const ShopContextProvider = (props) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
 
     if(localStorage.getItem('auth-token')){
-      fetch('http://localhost:4000/removefromcart', {
+      fetch('https://e-commerce-backend-irdi.onrender.com/removefromcart', {
         method:'POST',
         headers:{
           Accept:'application/form-data',
